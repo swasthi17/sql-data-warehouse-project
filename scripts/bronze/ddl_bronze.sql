8fc63d17e59c0210d8eecfb4f1c0cee753c3dd22
Swasthi Shetty <33115190+swasthi17@users.noreply.github.com>
2026-04-21 19:44:39 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,25 +1,25 @@
 /*
 ===============================================================================
-DDL Script: Create Silver Tables
+DDL Script: Create bronze Tables
 ===============================================================================
 Script Purpose:
-    This script creates tables in the 'silver' schema, dropping existing tables 
+    This script creates tables in the 'bronze' schema, dropping existing tables 
     if they already exist.
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
 
 -- Drop tables if they exist
-DROP TABLE IF EXISTS silver.crm_cust_info;
-DROP TABLE IF EXISTS silver.crm_prd_info;
-DROP TABLE IF EXISTS silver.crm_sales_details;
-DROP TABLE IF EXISTS silver.erp_loc_a101;
-DROP TABLE IF EXISTS silver.erp_cust_az12;
-DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 
 -- Create tables
 
-CREATE TABLE silver.crm_cust_info (
+CREATE TABLE bronze.crm_cust_info (
     cst_id             INT,
     cst_key            VARCHAR(50),
     cst_firstname      VARCHAR(50),
@@ -30,7 +30,7 @@ CREATE TABLE silver.crm_cust_info (
     dwh_create_date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE silver.crm_prd_info (
+CREATE TABLE bronze.crm_prd_info (
     prd_id          INT,
     cat_id          VARCHAR(50),
     prd_key         VARCHAR(50),
@@ -42,7 +42,7 @@ CREATE TABLE silver.crm_prd_info (
     dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE silver.crm_sales_details (
+CREATE TABLE bronze.crm_sales_details (
     sls_ord_num     VARCHAR(50),
     sls_prd_key     VARCHAR(50),
     sls_cust_id     INT,
@@ -55,20 +55,20 @@ CREATE TABLE silver.crm_sales_details (
     dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE silver.erp_loc_a101 (
+CREATE TABLE bronze.erp_loc_a101 (
     cid             VARCHAR(50),
     cntry           VARCHAR(50),
     dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE silver.erp_cust_az12 (
+CREATE TABLE bronze.erp_cust_az12 (
     cid             VARCHAR(50),
     bdate           DATE,
     gen             VARCHAR(50),
     dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE silver.erp_px_cat_g1v2 (
+CREATE TABLE bronze.erp_px_cat_g1v2 (
     id              VARCHAR(50),
     cat             VARCHAR(50),
     subcat          VARCHAR(50),
